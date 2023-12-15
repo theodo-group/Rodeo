a11b5203677cb00b0ca6dc45023fe34e143ca9f6
mamadoudicko <mamadoudicko100@gmail.com>
2023-12-15 14:48:35 +0100

    feat: display similarity score

--- rodeo/rif/components/image_displayer.py
+++ rodeo/rif/components/image_displayer.py
@@ -5,5 +5,6 @@ from utils.convert_base64_to_image import convert_base64_to_image
 def display_image(image):
     try:
         st.image(convert_base64_to_image(image["content"]), width=300)
+        st.write("Similarity", round(image["similarity"], 2))
     except:
-        st.write("Error", image["content"])
+        st.write("Error", image["id"])
