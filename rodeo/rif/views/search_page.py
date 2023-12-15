7b57ab09177e4ff4ef9991481d1b05411f96308c
mamadoudicko <mamadoudicko100@gmail.com>
2023-12-15 14:51:47 +0100

    feat: add similarity threshold selection

--- rodeo/rif/views/search_page.py
+++ rodeo/rif/views/search_page.py
@@ -9,9 +9,19 @@ def search_page():
 
     text_query = st.text_input("Which image are you looking for ?")
 
+    similarity_threshold = st.slider(
+        "Similarity threshold", min_value=0.0, max_value=1.0, value=0.5
+    )
+
     with st.spinner("Training ongoing"):
         if text_query:
             images = get_nearest_images(text_query=text_query)
+            images = [
+                image for image in images if image["similarity"] >= similarity_threshold
+            ]
+            if len(images) == 0:
+                st.write("No similar images found 😢")
+
             for i in range(len(images)):
                 image = images[i]
                 with grid[i % 2]:
